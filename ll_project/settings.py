29b2209fcef4ee6872d50c8078607355069f2fb4
Mostakim15 <mbmostakim5050@gmail.com>
2026-04-19 21:36:07 +0600

    Add PostgreSQL support

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1ubmg#bx9a(0o@09lcj5e%32p)vp025)20$w9t1+ktsjk@y68x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 import dj_database_url
 import os
